c01be3ff4b6a054f2b7c9169c6c4193a4c411c3e
Surya <surya.nata.aardhana@gmail.com>
2023-02-28 15:25:27 +0700

    style: login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" style="overflow-x: hidden">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Confess</title>
+    <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="row d-flex height">
+        <div class="col-5 bg">
+            <div class="w-100 ms-5 mt-3 mb-5">
+                <img src="../images/logoT.png" alt="illustrasi" width="20%" />
+            </div>
+            <div class="text-center">
+                <img src="../images/illust.png" alt="logo" width="60%" />
+            </div>
+        </div>
+        <div class="col-7">
+            <h1 class="w-100 fw-bold text-center mt-5 pt-5 fs-3">MASUK</h1>
+            <div class="card mt-5 p-4 border-start-0 border-end-0" style="margin: 0 140px">
+                <div class="card-body">
+                    <form action="" method="post" class="my-4">
+                        <div class="mb-4">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control p-2" id="username" />
+                        </div>
+                        <div class="mb-5">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control p-2" id="password" />
+                            <div class="text-end mt-4">
+                                <a href="# " class="form-text">lupa password?</a>
+                            </div>
+                        </div>
+                        <div class="text-start mt-5">
+                            <button type="submit" class="w-100 btn-color btn text-white btn-primary p-2">masuk</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
