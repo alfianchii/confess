36a54103b8483b48c0bd305d259734b3fc9771f7
alfianchii <pianaee379@gmail.com>
2023-03-01 07:25:04 +0700

    chore: implements Vite

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,10 +8,11 @@
     <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
-    <link rel="stylesheet" href="../css/style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap" rel="stylesheet" />
+    {{-- <link rel="stylesheet" href="../css/style.css" /> --}}
+    @vite('resources/css/app.css')
 </head>
 
 <body>
