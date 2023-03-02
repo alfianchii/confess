024eafd2569c23d25b3f4aa8bc8d38b52105f998
alfianchii <pianaee379@gmail.com>
2023-03-02 19:03:49 +0700

    style: add section content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,15 @@
+@extends('layouts.main')
 
+@section('content')
+    <div class="page-content">
+        <section class="row">
+            <div class="col-12 mb-3">
+
+            </div>
+
+            <div class="col-12 mb-3">
+
+            </div>
+        </section>
+    </div>
+@endsection
