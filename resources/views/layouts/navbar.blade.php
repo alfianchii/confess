071053758d92e675a4601c7155fe4e576c0ddaf7
alfianchii <pianaee379@gmail.com>
2023-03-02 17:10:58 +0700

    style: navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,1 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container">
+        <a class="navbar-brand text-muted" href="/">Confess</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav me-auto mb-2 mb-lg-0">
 
+            </ul>
+
+            <ul class="nav">
+
+            </ul>
+        </div>
+    </div>
+</nav>
