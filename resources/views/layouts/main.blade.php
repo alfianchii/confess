aa1b96512520d357bd17580de358bb000e0c00a3
alfianchii <pianaee379@gmail.com>
2023-03-02 17:24:44 +0700

    chore: add main title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Main</title>
+    <title>{{ config('app.name') }} | {{ $title }}</title>
 </head>
 
 <body>
