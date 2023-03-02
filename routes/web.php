5cffd414577360b405b020b944398cf0ddca83ed
alfianchii <pianaee379@gmail.com>
2023-03-02 17:44:08 +0700

    chore: rename welcome to home

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome', ["title" => "Welcome!"]);
+    return view('home', ["title" => "Welcome!"]);
 });
 
 Route::get('/about', function () {
