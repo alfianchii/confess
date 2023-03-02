32d73412221da6e4206b54b7b30aea0792995ba1
alfianchii <pianaee379@gmail.com>
2023-03-02 17:22:04 +0700

    style: about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,62 @@
+@extends('layouts.main')
 
+@section('content')
+    <div class="page-content">
+        <section class="row">
+            <div class="col-12 mb-3">
+                <h2>Tentang</h2>
+            </div>
+
+            <div class="col-12 mb-3">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
+                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
+                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
+                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
+                    sapiente
+                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
+                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
+                    aperiam
+                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
+                    repellendus,
+                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
+                    dolorum. Amet nihil quisquam ab ipsam!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
+                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
+                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
+                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
+                    sapiente
+                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
+                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
+                    aperiam
+                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
+                    repellendus,
+                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
+                    dolorum. Amet nihil quisquam ab ipsam!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
+                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
+                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
+                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
+                    sapiente
+                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
+                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
+                    aperiam
+                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
+                    repellendus,
+                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
+                    dolorum. Amet nihil quisquam ab ipsam!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
+                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
+                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
+                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
+                    sapiente
+                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
+                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
+                    aperiam
+                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
+                    repellendus,
+                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
+                    dolorum. Amet nihil quisquam ab ipsam!</p>
+            </div>
+        </section>
+    </div>
+@endsection
