36fcd8b7887e0ecd7503f593cd3fa676f123ace1
alfianchii <pianaee379@gmail.com>
2023-03-02 19:04:26 +0700

    style: add page heading

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,8 @@
     <div class="page-content">
         <section class="row">
             <div class="col-12 mb-3">
-
+                <h2>Login</h2>
+                <p class="text-subtitle">Masuk dengan akun yang sudah didaftarkan oleh Admin.</p>
             </div>
 
             <div class="col-12 mb-3">
