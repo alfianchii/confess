9c46263e05b719b6338f715ad2678728ebe70a08
alfianchii <pianaee379@gmail.com>
2023-03-02 19:05:49 +0700

    style: login card's header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,11 +12,10 @@
                 <section class="section text-center">
                     <div class="card">
                         <div class="card-header">
-
+                            <h3 class="card-title">Akun Anda</h3>
                         </div>
 
                         <div class="card-body">
-
                         </div>
                     </div>
                 </section>
