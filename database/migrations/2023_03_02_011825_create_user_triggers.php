cf5f7a5286aeff4a0955aaf2b1ff398a0a274a3b
alfianchii <pianaee379@gmail.com>
2023-03-02 08:43:45 +0700

    add: users trigger query

--- database/migrations/2023_03_02_011825_create_user_triggers.php
+++ database/migrations/2023_03_02_011825_create_user_triggers.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,6 +14,20 @@ return new class extends Migration
      */
     public function up()
     {
+        DB::unprepared('
+            CREATE TRIGGER tr_add_user 
+            AFTER INSERT
+            ON users FOR EACH ROW
+            BEGIN
+                IF NEW.level = "student" THEN
+                    INSERT INTO students (user_nik)
+                    VALUES (NEW.nik);
+                ELSEIF NEW.level = "officer" OR NEW.level = "admin" THEN
+                    INSERT INTO officers (user_nik)
+                    VALUES (NEW.nik);
+                END IF;
+            END;
+        ');
     }
 
     /**
@@ -22,5 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
+        DB::unprepared("DROP TRIGGER tr_add_user");
     }
 };
