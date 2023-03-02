1dfa0d1e7f53b67f001f738671e3095c596cadc8
alfianchii <pianaee379@gmail.com>
2023-03-02 17:36:37 +0700

    style: Beranda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,62 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.main')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+    <div class="page-content">
+        <section class="row">
+            <div class="col-12 mb-3">
+                <h2>Beranda</h2>
+            </div>
 
-    <title>{{ config('app.name') }}</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
-</head>
-
-<body>
-    <h1>Hi, everyone!</h1>
-</body>
-
-</html>
+            <div class="col-12 mb-3">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
+                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
+                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
+                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
+                    sapiente
+                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
+                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
+                    aperiam
+                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
+                    repellendus,
+                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
+                    dolorum. Amet nihil quisquam ab ipsam!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
+                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
+                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
+                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
+                    sapiente
+                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
+                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
+                    aperiam
+                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
+                    repellendus,
+                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
+                    dolorum. Amet nihil quisquam ab ipsam!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
+                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
+                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
+                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
+                    sapiente
+                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
+                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
+                    aperiam
+                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
+                    repellendus,
+                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
+                    dolorum. Amet nihil quisquam ab ipsam!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
+                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
+                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
+                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
+                    sapiente
+                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
+                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
+                    aperiam
+                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
+                    repellendus,
+                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
+                    dolorum. Amet nihil quisquam ab ipsam!</p>
+            </div>
+        </section>
+    </div>
+@endsection
