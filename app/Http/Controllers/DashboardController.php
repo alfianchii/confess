63a6a2d2cfea5d8c9f0b16b4924557e6d4f17ca4
alfianchii <pianaee379@gmail.com>
2023-03-03 16:33:29 +0700

    chore: replace dashboard.layouts.main to dashboard.index

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view("dashboard.layouts.main", ["title" => "Dashboard"]);
+        return view("dashboard.index", ["title" => "Dashboard"]);
     }
 
     /**
