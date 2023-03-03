d57c4bd2f3a2e654fc7a80505237e07066f67c76
alfianchii <pianaee379@gmail.com>
2023-03-03 17:23:10 +0700

    chore: regist student middleware in kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\IsAdmin::class,
         'officer' => \App\Http\Middleware\IsOfficer::class,
+        'student' => \App\Http\Middleware\IsStudent::class,
     ];
 }
