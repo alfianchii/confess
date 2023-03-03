a61bffe2e60eb3a88ecc2b8dd7d19ff2b9798d76
Surya <surya.nata.aardhana@gmail.com>
2023-03-03 16:35:23 +0700

    style: footer login

---
+++ resources/views/auth/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="mt-3 mt-sm-5">
+    <div class="footer clearfix mb-sm-0 mb-3 text-muted ps-3">
+        <div class="float-start">
+            <small class="mb-sm-0">2023 &copy; Confess. All rights reserved</small>
+        </div>
+    </div>
+</footer>
