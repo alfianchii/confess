54c4bf138c3802c50b72a15a52588ae63836d19a
alfianchii <pianaee379@gmail.com>
2023-03-03 16:58:01 +0700

    chore: sidebar image logo

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
         <div class="sidebar-header position-relative">
             <div class="d-flex justify-content-between align-items-center">
                 <div class="logo">
-                    <a href="/"><img src="assets/images/logo/logo.svg" alt="Logo" srcset="" /></a>
+                    <a href="/"><img src="../images/logoT.png" alt="Logo" srcset="" width="80" /></a>
                 </div>
                 <div class="theme-toggle d-flex gap-2 align-items-center mt-2">
                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
