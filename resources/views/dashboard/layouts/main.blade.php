ea770632afb1d7938cba10ceabda37f9858ea53b
alfianchii <pianaee379@gmail.com>
2023-03-03 15:58:48 +0700

    style: main dashboard links

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ config('app.name') }} | {{ $title }}</title>
+
+    @include('partials.link')
 </head>
 
 <body>
