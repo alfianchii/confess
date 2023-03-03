02ca7811d1897e6aa451f42e371695948e0f7ee0
alfianchii <pianaee379@gmail.com>
2023-03-03 17:22:00 +0700

    fix: relative path link

--- resources/views/auth/partials/link.blade.php
+++ resources/views/auth/partials/link.blade.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="assets/css/main/app.css" />
-<link rel="stylesheet" href="assets/css/main/app-dark.css" />
-<link rel="stylesheet" href="assets/css/shared/iconly.css" />
+<link rel="stylesheet" href="../assets/css/main/app.css" />
+<link rel="stylesheet" href="../assets/css/main/app-dark.css" />
+<link rel="stylesheet" href="../assets/css/shared/iconly.css" />
 <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon" />
 @vite('resources/css/app.css')
--- resources/views/partials/link.blade.php
+++ resources/views/partials/link.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="assets/css/main/app.css" />
-<link rel="stylesheet" href="assets/css/main/app-dark.css" />
-<link rel="stylesheet" href="assets/css/shared/iconly.css" />
+<link rel="stylesheet" href="../assets/css/main/app.css" />
+<link rel="stylesheet" href="../assets/css/main/app-dark.css" />
+<link rel="stylesheet" href="../assets/css/shared/iconly.css" />
 <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon" />
