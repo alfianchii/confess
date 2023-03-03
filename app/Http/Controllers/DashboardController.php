37d22a6614e69248e0f5f1c2d8e8275b69a45dc0
alfianchii <pianaee379@gmail.com>
2023-03-03 15:53:06 +0700

    feat: dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,6 +13,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        return view("dashboard.layouts.main", ["title" => "Dashboard"]);
     }
 
     /**
