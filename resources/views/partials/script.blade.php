7bf8006851733ac4aec88a735ef0c8ed907479a2
Surya <surya.nata.aardhana@gmail.com>
2023-03-04 17:42:53 +0700

    chore: add @vite('resources/js/navbar.js')

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -2,3 +2,4 @@
 <script src="../assets/js/app.js"></script>
 <script src="../assets/extensions/apexcharts/apexcharts.min.js"></script>
 <script src="../assets/js/pages/dashboard.js"></script>
+@vite('resources/js/navbar.js')
