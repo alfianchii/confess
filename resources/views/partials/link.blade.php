7ab023fea706a915120603bd089a327b3d03e1c9
Surya <surya.nata.aardhana@gmail.com>
2023-03-04 15:10:15 +0700

    chore:add vite('resources/css/app.css')

--- resources/views/partials/link.blade.php
+++ resources/views/partials/link.blade.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" href="../assets/css/main/app-dark.css" />
 <link rel="stylesheet" href="../assets/css/shared/iconly.css" />
 <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon" />
+@vite('resources/css/app.css')
