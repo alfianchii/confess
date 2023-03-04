6d986a41716229099f7b5c0f599f9399b3eb4b3d
Surya <surya.nata.aardhana@gmail.com>
2023-03-04 17:07:13 +0700

    style: about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,59 +3,44 @@
 @section('content')
     <div class="page-content">
         <section class="row">
-            <div class="col-12 mb-3">
-                <h2>Tentang</h2>
+            <div class="col-12 mb-3 header-about bg-about">
+                <div class="container text-center mt-4 mt-sm-5">
+                    <h1 class="text-white">Apa itu confess?</h1>
+                    <img src="../images/about.png" alt="about illustration" class="illust-about">
+                </div>
             </div>
 
             <div class="col-12">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
-                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
-                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
-                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
-                    sapiente
-                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
-                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
-                    aperiam
-                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
-                    repellendus,
-                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
-                    dolorum. Amet nihil quisquam ab ipsam!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
-                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
-                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
-                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
-                    sapiente
-                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
-                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
-                    aperiam
-                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
-                    repellendus,
-                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
-                    dolorum. Amet nihil quisquam ab ipsam!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
-                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
-                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
-                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
-                    sapiente
-                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
-                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
-                    aperiam
-                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
-                    repellendus,
-                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
-                    dolorum. Amet nihil quisquam ab ipsam!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id sequi quidem cumque iusto
-                    laborum alias ut modi minima expedita sed? Natus earum ipsum, iusto veritatis quidem
-                    pariatur soluta cum dolore, quis mollitia dolorem neque debitis quisquam illo sint omnis
-                    atque labore voluptates ex odio obcaecati saepe optio repellendus? Quas dicta porro
-                    sapiente
-                    labore repellendus praesentium suscipit tempora, velit quaerat aut aperiam perferendis
-                    eveniet? Quisquam reprehenderit hic earum dolore ullam reiciendis. Voluptatem labore
-                    aperiam
-                    quidem incidunt dignissimos iusto inventore voluptatibus quia nihil nemo error
-                    repellendus,
-                    dolorem necessitatibus ipsa unde voluptates perferendis optio, impedit officia ipsam
-                    dolorum. Amet nihil quisquam ab ipsam!</p>
+                <div class="container mt-4 text-center ">
+                    <p class="fs-5 px-sm-5">Confess merupakan sebuah wadah bagi siswa/i SMKN 4 Kota Tangerang. Dimana
+                        pada aplikasi
+                        ini siswa
+                        dapat menyampaikan Pengaduan dan aspirasi nya dengan terbuka dan aman. Harapan nya dengan ada nya
+                        confess, Siswa/i akan lebih berani dan terbuka kepada sekolah untuk menyelesaikan masalah nya. </p>
+                    <h2 class="mt-5 mb-4">
+                        Kenapa diciptakan nya Confess?
+                    </h2>
+                    <p class="fs-5 px-sm-5">Pengelolaan pengaduan pelayanan di sekolah belum terkelola secara efektif dan
+                        terintegrasi.
+                        Masing-masing organisasi penyelenggara mengelola pengaduan secara parsial dan tidak terkoordinir
+                        dengan baik. Akibatnya terjadi duplikasi penanganan pengaduan, atau bahkan bisa terjadi suatu
+                        pengaduan tidak ditangani oleh satupun organisasi penyelenggara, dengan alasan pengaduan bukan
+                        kewenangannya. Oleh karena itu, untuk mencapai visi dalam good governance maka perlu untuk
+                        mengintegrasikan sistem pengelolaan pengaduan pelayanan publik dalam satu pintu. Tujuannya, Warga
+                        sekolah memiliki satu saluran pengaduan secara Nasional.
+                    </p>
+
+                    <div class="text-center">
+                        <h2 class=" my-5">
+                            Lokasi Kami
+                        </h2>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.5768925963066!2d106.63556391455468!3d-6.187333395520691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f929162547c7%3A0xbbf35137362e584d!2sSMK%20Negeri%204%20Kota%20Tangerang!5e0!3m2!1sid!2sid!4v1677921080826!5m2!1sid!2sid"
+                            width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade">
+                        </iframe>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
