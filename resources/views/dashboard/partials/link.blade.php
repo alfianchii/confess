3970064e5f841397b7a42d4044f8ae53f000e693
alfianchii <pianaee379@gmail.com>
2023-03-06 22:27:27 +0700

    style: add dashboard's base links

---
+++ resources/views/dashboard/partials/link.blade.php
@@ -0,0 +1,5 @@
+{{-- Base links --}}
+<link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/css/main/app-dark.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}" />
+<link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon" />
