82facb4ac387fccedb6145d14bb0c8bf84835494
alfianchii <pianaee379@gmail.com>
2023-03-06 22:17:10 +0700

    style: complaint index's scripts

--- resources/views/dashboard/complaints/index.blade.php
+++ resources/views/dashboard/complaints/index.blade.php
@@ -12,4 +12,10 @@
 @endsection
 
 @section('scripts')
+    {{-- SweetAlert --}}
+    <script src="{{ asset('assets/extensions/sweetalert2/sweetalert2.min.js') }}"></script>
+    @vite(['resources/js/sweetalert/swalMulti.js'])
+    {{-- Simple DataTable --}}
+    <script src="{{ asset('assets/extensions/simple-datatables/umd/simple-datatables.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/simple-datatables.js') }}"></script>
 @endsection
