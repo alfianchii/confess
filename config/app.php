42a977951e70ded6014fff10c4554f17714714de
alfianchii <pianaee379@gmail.com>
2023-03-06 16:25:44 +0700

    fix: RealRashid\SweetAlert package in app.php

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
+        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -194,7 +195,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
     ],
 
     /*
