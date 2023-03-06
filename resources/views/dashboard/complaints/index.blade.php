25850154641ea0c33a301ed953e28ba47112f456
alfianchii <pianaee379@gmail.com>
2023-03-06 22:16:51 +0700

    style: complaint index's styles

---
+++ resources/views/dashboard/complaints/index.blade.php
@@ -0,0 +1,15 @@
+@extends('dashboard.layouts.main')
+
+@section('links')
+    {{-- Simple DataTable --}}
+    <link rel="stylesheet" href="{{ asset('assets/extensions/simple-datatables/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/pages/simple-datatables.css') }}" />
+    {{-- Sweetalert --}}
+    <link rel="stylesheet" href="{{ asset('assets/extensions/sweetalert2/sweetalert2.min.css') }}" />
+@endsection
+
+@section('content')
+@endsection
+
+@section('scripts')
+@endsection
