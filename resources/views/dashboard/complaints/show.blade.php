2ce09fe02876d94471a628ccaa23d8b6c1e323e6
alfianchii <pianaee379@gmail.com>
2023-03-06 22:19:47 +0700

    style: add complaint show's styles

--- resources/views/dashboard/complaints/show.blade.php
+++ resources/views/dashboard/complaints/show.blade.php
@@ -1,6 +1,10 @@
 @extends('dashboard.layouts.main')
 
 @section('links')
+    {{-- SweetAlert --}}
+    <link rel="stylesheet" href="{{ asset('assets/extensions/sweetalert2/sweetalert2.min.css') }}" />
+    {{-- Quill --}}
+    <link rel="stylesheet" href="{{ asset('assets/extensions/quill/quill.snow.css') }}" />
 @endsection
 
 @section('content')
