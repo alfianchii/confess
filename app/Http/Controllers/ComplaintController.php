ad1bebbc05193cd2e2170e832f44caad12530d93
alfianchii <pianaee379@gmail.com>
2023-03-06 22:14:21 +0700

    chore: add ComplaintController

---
+++ app/Http/Controllers/ComplaintController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Complaint;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Cviebrock\EloquentSluggable\Services\SlugService;
+
+class ComplaintController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Complaint  $complaint
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Complaint $complaint)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Complaint  $complaint
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Complaint $complaint)
+    {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Complaint  $complaint
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Complaint $complaint)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Complaint  $complaint
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Complaint $complaint)
+    {
+        if ($complaint->image) {
+            Storage::delete($complaint->image);
+        }
+
+        try {
+            Complaint::destroy($complaint->id);
+        } catch (\Exception $e) {
+            return response()->json([
+                "message" => "Gagal untuk menghapus keluhan."
+            ], 422);
+        }
+
+        return response()->json([
+            "message" => "Keluhan kamu telah dihapus!"
+        ], 200);
+    }
+
+    public function checkSlug(Request $request)
+    {
+    }
+}
