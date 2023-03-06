289db66748894f03392251159f2d1de59e0a9981
alfianchii <pianaee379@gmail.com>
2023-03-06 21:44:39 +0700

    chore: add csrf_token()

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name') }} | {{ $title }}</title>
 
     @include('partials.link')
