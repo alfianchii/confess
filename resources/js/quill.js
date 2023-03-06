6d401a6894b9955592bdcf9fdfea37ae840202ba
alfianchii <pianaee379@gmail.com>
2023-03-06 22:29:31 +0700

    feat: quill

---
+++ resources/js/quill.js
@@ -0,0 +1,63 @@
+let quill = new Quill("#editor", {
+    bounds: "#editor-container .editor",
+    modules: {
+        toolbar: [
+            [
+                {
+                    font: [],
+                },
+                {
+                    size: [],
+                },
+            ],
+            ["bold", "italic", "underline", "strike"],
+            [
+                {
+                    color: [],
+                },
+                {
+                    background: [],
+                },
+            ],
+            ["blockquote", "code-block"],
+            [
+                {
+                    script: "super",
+                },
+                {
+                    script: "sub",
+                },
+            ],
+            [
+                {
+                    list: "ordered",
+                },
+                {
+                    list: "bullet",
+                },
+                {
+                    indent: "-1",
+                },
+                {
+                    indent: "+1",
+                },
+            ],
+            [
+                "direction",
+                {
+                    align: [],
+                },
+            ],
+            ["link"],
+            ["clean"],
+        ],
+    },
+    theme: "snow",
+    placeholder: "Tuliskan keluhan kamu di sini ...",
+});
+
+function updateBody() {
+    document.getElementById("body").value = quill.root.innerHTML;
+}
+
+quill.on("text-change", updateBody);
