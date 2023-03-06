70e4d0befe26f6bffb1f910ab433fb234f4abaf5
alfianchii <pianaee379@gmail.com>
2023-03-06 22:24:26 +0700

    style: add complaint create's links

--- resources/views/dashboard/complaints/create.blade.php
+++ resources/views/dashboard/complaints/create.blade.php
@@ -1,6 +1,11 @@
 @extends('dashboard.layouts.main')
 
 @section('links')
+    {{-- Quill --}}
+    <link rel="stylesheet" href="{{ asset('assets/extensions/quill/quill.snow.css') }}" />
+
+    {{-- Form: select option --}}
+    <link rel="stylesheet" href="{{ asset('assets/extensions/choices.js/public/assets/styles/choices.css') }}">
 @endsection
 
 @section('content')
