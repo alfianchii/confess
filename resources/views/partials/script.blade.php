14dd79cbfde45264e07f52070c8d3e16fd5d93f3
alfianchii <pianaee379@gmail.com>
2023-03-06 21:39:05 +0700

    chore: fix script's relative paths

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1,4 +1,4 @@
-<script src="../assets/js/bootstrap.js"></script>
-<script src="../assets/js/app.js"></script>
-<script src="../assets/extensions/apexcharts/apexcharts.min.js"></script>
-<script src="../assets/js/pages/dashboard.js"></script>
+<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
+<script src="{{ asset('assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/js/pages/dashboard.js') }}"></script>
