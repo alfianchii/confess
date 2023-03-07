4cef61c6a0f81c03bd74faa88ab35ed7af25e162
alfianchii <pianaee379@gmail.com>
2023-03-07 07:13:13 +0700

    style: add sweetalert in main auth's link

--- resources/views/auth/partials/link.blade.php
+++ resources/views/auth/partials/link.blade.php
@@ -2,4 +2,6 @@
 <link rel="stylesheet" href="{{ asset('assets/css/main/app-dark.css') }}" />
 <link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}" />
 <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon" />
-@vite('resources/css/app.css')
+{{-- Sweetalert --}}
+<link rel="stylesheet" href="{{ asset('assets/extensions/sweetalert2/sweetalert2.min.css') }}" />
+@vite(['resources/css/app.css'])
