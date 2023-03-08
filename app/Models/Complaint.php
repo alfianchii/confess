5aacdc7311c2306735e64805b58363185a7409b0
alfianchii <pianaee379@gmail.com>
2023-03-08 22:38:03 +0700

    fix: complaint model and footer typography

--- app/Models/Complaint.php
+++ app/Models/Complaint.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Complaint extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,6 +18,26 @@ class Complaint extends Model
     protected $guarded = [
         'id',
     ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    // protected $with = [
+    //     'student',
+    //     'category',
+    //     'responses',
+    // ];
 
     public function getRouteKeyName()
     {
