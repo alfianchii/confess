ceb487fb844756232ac1144e4d85b8c69c9f6a3c
Surya <surya.nata.aardhana@gmail.com>
2023-03-08 14:12:38 +0700

    style:home page

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -3,11 +3,11 @@ const hamburgerBtn = document.querySelector(".navbar-toggler");
 hamburgerBtn.addEventListener("click", function () {
     navBar.classList.toggle("active");
 });
-function scroll(e) {
+function scroll() {
     let calc = window.scrollY;
-    if (calc > 380) {
+    if (calc > 180) {
         navBar.classList.replace("bg-transparent", "bg-nav");
-    } else if (calc <= 380) {
+    } else if (calc <= 180) {
         navBar.classList.replace("bg-nav", "bg-transparent");
     }
 }
