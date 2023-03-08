18a823e1945410e347afbeddb4c3107777c2e2fa
alfianchii <pianaee379@gmail.com>
2023-03-08 20:35:22 +0700

    feat: ResponseController

---
+++ app/Http/Controllers/ResponseController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Response;
+use Illuminate\Http\Request;
+
+class ResponseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("dashboard.responses.index", [
+            "title" => "Responses",
+            "responses" => Response::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Response $response)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Response $response)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Response $response)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Response $response)
+    {
+        //
+    }
+}
