672da4793cc3ed48f920366a360edae8563f17dd
alfianchii <pianaee379@gmail.com>
2023-03-15 20:05:44 +0700

    fix: remove links and scripts in response's edit

--- resources/views/dashboard/responses/edit.blade.php
+++ resources/views/dashboard/responses/edit.blade.php
@@ -6,9 +6,6 @@
 
     {{-- Quill --}}
     <link rel="stylesheet" href="{{ asset('assets/extensions/quill/quill.snow.css') }}" />
-
-    {{-- Form: select option --}}
-    <link rel="stylesheet" href="{{ asset('assets/extensions/choices.js/public/assets/styles/choices.css') }}">
 @endsection
 
 @section('content')
@@ -164,14 +161,6 @@
     {{-- Quill --}}
     @vite(['resources/js/quill.js'])
     <script src="{{ asset('assets/extensions/quill/quill.min.js') }}"></script>
-    {{-- Jquery --}}
-    <script src="{{ asset('assets/extensions/jquery/jquery.min.js') }}"></script>
-    {{-- Form: parsley --}}
-    <script src="{{ asset('assets/extensions/parsleyjs/parsley.min.js') }}"></script>
-    <script src="{{ asset('assets/js/pages/parsley.js') }}"></script>
-    {{-- Form: select option --}}
-    <script src="{{ asset('assets/extensions/choices.js/public/assets/scripts/choices.js') }}"></script>
-    <script src="{{ asset('assets/js/pages/form-element-select.js') }}"></script>
     {{-- SweetAlert --}}
     @vite(['resources/js/sweetalert/swalSingle.js'])
     <script src="{{ asset('assets/extensions/sweetalert2/sweetalert2.min.js') }}"></script>
