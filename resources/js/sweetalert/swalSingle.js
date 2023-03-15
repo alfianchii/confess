855606846326cb6da72b1ef9e77199fd217934c4
alfianchii <pianaee379@gmail.com>
2023-03-15 09:18:19 +0700

    fix: single and multi sweetalert with current path

--- resources/js/sweetalert/swalSingle.js
+++ resources/js/sweetalert/swalSingle.js
@@ -1,9 +1,24 @@
 import { handleDelete } from "../sweetalert";
 
+// Path
+const currentPath = window.location.pathname;
+
 // Single delete button
 const buttons = document.getElementsByClassName("delete-record");
 for (const btn of buttons) {
     btn.addEventListener("click", function (e) {
-        handleDelete(e.target.dataset.slug);
+        if (currentPath.startsWith("/dashboard/complaints")) {
+            handleDelete(
+                e.target.dataset.slug,
+                "keluhan",
+                "/dashboard/complaints"
+            );
+        } else if (currentPath.startsWith("/dashboard/responses")) {
+            handleDelete(
+                e.target.dataset.slug,
+                "tanggapan",
+                "/dashboard/responses"
+            );
+        }
     });
 }
