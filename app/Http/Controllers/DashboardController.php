02cac962f7cbdcf674529f9152fbbe31e534ef07
alfianchii <pianaee379@gmail.com>
2023-03-19 19:32:43 +0700

    feat: responsesData method in DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -96,4 +96,62 @@ class DashboardController extends Controller
     {
         //
     }
+
+    public function responsesData()
+    {
+        // Retrieve responses created in the last 7 days
+        $responses = DB::table('responses')
+            ->where("responses.officer_nik", auth()->user()->nik)
+            ->whereBetween('responses.created_at', [Carbon::now()->subDays(6), Carbon::now()])
+            // Select the date of creation and count of responses for each day
+            ->select(
+                DB::raw('DATE(responses.created_at) as date'),
+                DB::raw('COUNT(*) as count'),
+            )
+            // Group the results by the date of creation
+            ->groupBy('date')
+            // Order the results by date in ascending order
+            ->orderBy('date', 'asc')
+            // Execute the query and retrieve the results
+            ->get();
+
+        // Create an array with the dates for the last 7 days
+        $dateRange = Carbon::now()->subDays(6)->toPeriod(Carbon::now());
+
+        // Create an array to store the response counts for each day
+        $xAxis = [];
+        $yAxis = [];
+
+        // Loop through the date range and populate the counts array with the response counts
+        foreach ($dateRange as $date) {
+            $formattedDate = $date->format('Y-m-d');
+            $count = $responses->where('date', $formattedDate)->pluck('count')->first();
+            $yAxis[] = $count ?? 0; // Use 0 if the count is null
+            $xAxis[] = $date->format("Y-m-d");
+        }
+
+        // Retrieve genders which based on responses
+        $genders = DB::table('responses')
+            ->join("officers", 'responses.officer_nik', "=", "officers.officer_nik")
+            ->join("users", 'officers.officer_nik', "=", "users.nik")
+            ->select(
+                DB::raw('SUM(CASE WHEN users.gender = "L" THEN 1 ELSE 0 END) as male'),
+                DB::raw('SUM(CASE WHEN users.gender = "P" THEN 1 ELSE 0 END) as female'),
+            )->get()[0];
+
+        $chartData = [
+            // Use the response counts as chart data
+            'data' => [
+                // Records of responses
+                "yAxis" => $yAxis,
+                // Labels of responses
+                "xAxis" => $xAxis,
+                // Genders of responses
+                "genders" => $genders
+            ],
+        ];
+
+        // Return the chart data as a JSON response
+        return response()->json($chartData);
+    }
 }
