c11252b28bdde0dae2c02cf701a3fda44d9a594b
alfianchii <pianaee379@gmail.com>
2023-03-19 19:44:51 +0700

    style: sidebar profile

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -196,6 +196,23 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-12 col-lg-3">
+                    <div class="card">
+                        <div class="card-body py-4 px-4">
+                            <div class="d-flex align-items-center">
+                                <div class="avatar avatar-xl">
+                                    <img src="assets/images/faces/1.jpg" alt="Face 1" />
+                                </div>
+                                <div class="ms-3 name">
+                                    <h5 class="font-bold">{{ auth()->user()->name }}</h5>
+                                    <h6 class="text-muted mb-0">
+                                        {{ htmlspecialchars('@' . auth()->user()->username) }}
+                                    </h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     </div>
