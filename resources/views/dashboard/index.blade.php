677aae8d129fdf0f2cd13b2ff68a12557b17ec08
alfianchii <pianaee379@gmail.com>
2023-03-19 19:41:17 +0700

    style: statistics

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -24,43 +24,98 @@
                 </div>
             </div>
         </div>
-        <section class="section">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">About Vertical Navbar</h4>
+        <div class="page-content mt-4">
+            <section class="row">
+                <div class="col-12 col-lg-9">
+                    <div class="row">
+                        <div class="col-6 col-lg-3 col-md-6">
+                            <div class="card">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                            <div class="stats-icon purple mb-2">
+                                                <i class="iconly-boldShow"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                            <h6 class="text-muted font-semibold">
+                                                Keluhan
+                                            </h6>
+                                            <h6 class="font-extrabold mb-0">
+                                                {{ $complaints->count() }}
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3 col-md-6">
+                            <div class="card">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                            <div class="stats-icon blue mb-2">
+                                                <i class="iconly-boldProfile"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                            <h6 class="text-muted font-semibold">
+                                                Pegawai
+                                            </h6>
+                                            <h6 class="font-extrabold mb-0">
+                                                {{ $officers->count() }}
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3 col-md-6">
+                            <div class="card">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                            <div class="stats-icon green mb-2">
+                                                <i class="iconly-boldAdd-User"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                            <h6 class="text-muted font-semibold">
+                                                Murid
+                                            </h6>
+                                            <h6 class="font-extrabold mb-0">
+                                                {{ $students->count() }}
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-lg-3 col-md-6">
+                            <div class="card">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
+                                            <div class="stats-icon red mb-2">
+                                                <i class="iconly-boldBookmark"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                            <h6 class="text-muted font-semibold">
+                                                Tanggapan
+                                            </h6>
+                                            <h6 class="font-extrabold mb-0">
+                                                {{ $responses->count() }}
+                                            </h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <p>
-                        Vertical Navbar is a layout option that you can use with
-                        Mazer.
-                    </p>
-
-                    <p>
-                        In case you want the navbar to be sticky on top while
-                        scrolling, add <code>.navbar-fixed</code> class alongside
-                        with <code>.layout-navbar</code> class.
-                    </p>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">About Vertical Navbar</h4>
-                </div>
-                <div class="card-body">
-                    <p>
-                        Vertical Navbar is a layout option that you can use with
-                        Mazer.
-                    </p>
-
-                    <p>
-                        In case you want the navbar to be sticky on top while
-                        scrolling, add <code>.navbar-fixed</code> class alongside
-                        with <code>.layout-navbar</code> class.
-                    </p>
-                </div>
-            </div>
-        </section>
-    </div>
+            </section>
+        </div>
     </div>
 @endsection
 
