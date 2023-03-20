d25b62ae816fd00c8162f93c0380385bb697e286
alfianchii <pianaee379@gmail.com>
2023-03-20 12:26:06 +0700

    feat(route authorization): ResponseController show method

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -75,6 +75,11 @@ class ResponseController extends Controller
      */
     public function show(Response $response)
     {
+        // Validate if the response is owned by the user
+        if ($response->officer_nik !== auth()->user()->nik) {
+            return redirect('/dashboard/responses')->withErrors('Kamu bukan pemilik dari tanggapan tersebut.');
+        }
+
         return view("dashboard.responses.show", [
             "title" => "Tanggapan",
             "response" => $response,
