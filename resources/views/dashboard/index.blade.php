d1f950d3d3951bd8601564d17bc1dd5ebf85cb48
alfianchii <pianaee379@gmail.com>
2023-03-20 19:58:33 +0700

    feat: officer dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -777,6 +777,79 @@
                 chartYourResponses.render()
                 chartAllResponsesGender.render()
                 chartComplaintAndResponse.render()
+            } else if (userLevel === "officer") {
+                // Set options
+                var optionsYourResponses = {
+                    annotations: {
+                        position: "back",
+                    },
+                    dataLabels: {
+                        enabled: false,
+                    },
+                    chart: {
+                        type: "bar",
+                        height: 300,
+                    },
+                    fill: {
+                        opacity: 1,
+                    },
+                    plotOptions: {},
+                    series: [{
+                        name: "tanggapan",
+                        data: responses.yAxis,
+                    }, ],
+                    colors: "#435ebe",
+                    xaxis: {
+                        categories: responses.xAxis,
+                        type: "datetime",
+                    },
+                    yaxis: {
+                        labels: {
+                            formatter: function(value) {
+                                return Math.round(value);
+                            }
+                        }
+                    },
+                    tooltip: {
+                        x: {
+                            format: "dd/MM/yy",
+                        },
+                    },
+                }
+                let optionsAllResponsesGender = {
+                    series: [allResponsesGender.male, allResponsesGender.female],
+                    labels: ["Male", "Female"],
+                    colors: ["#435ebe", "#55c6e8"],
+                    chart: {
+                        type: "donut",
+                        width: "100%",
+                        height: "350px",
+                    },
+                    legend: {
+                        position: "bottom",
+                    },
+                    plotOptions: {
+                        pie: {
+                            donut: {
+                                size: "30%",
+                            },
+                        },
+                    },
+                }
+
+                // Instance chart
+                var chartYourResponses = new ApexCharts(
+                    document.querySelector("#chart-your-responses"),
+                    optionsYourResponses
+                )
+                var chartAllResponsesGender = new ApexCharts(
+                    document.getElementById("chart-visitors-profile"),
+                    optionsAllResponsesGender
+                )
+
+                // Render
+                chartYourResponses.render()
+                chartAllResponsesGender.render()
             }
         })
     </script>
