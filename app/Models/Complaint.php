2e8161d29544500148f4b5bc463c8f6c5385f26f
alfianchii <pianaee379@gmail.com>
2023-03-23 13:28:35 +0700

    feat: allComplaintAxises() in Response

--- app/Models/Complaint.php
+++ app/Models/Complaint.php
@@ -96,4 +96,58 @@ class Complaint extends Model
 
         return $axises;
     }
+
+    public static function allComplaintAxises()
+    {
+        // All Complaints
+        $allComplaints = Complaint::whereBetween('complaints.created_at', [Carbon::now()->subDays(7), Carbon::now()])
+            // Select the date of creation and count of complaints for each day
+            ->selectRaw("DATE(complaints.created_at) as date, COUNT(*) as count")
+            // Group the results by the date of creation
+            ->groupBy('date', 'complaints.created_at')
+            // Order the results by date in ascending order
+            ->orderBy('date', 'asc')
+            // Execute the query and retrieve the results
+            ->get();
+
+        // Genders
+        $genders = Complaint::join("students", 'complaints.student_nik', "=", "students.student_nik")
+            ->join("users", 'students.student_nik', "=", "users.nik")
+            ->selectRaw('SUM(CASE WHEN users.gender = "L" THEN 1 ELSE 0 END) as male')
+            ->selectRaw('SUM(CASE WHEN users.gender = "P" THEN 1 ELSE 0 END) as female')
+            ->first()->attributes;
+
+        // Create an array to store the all complaints' data
+        $axises = [
+            "data" => [
+                'xAxis' => [],
+                'yAxis' => [],
+            ],
+            "genders" => [
+                'male' => 0,
+                'female' => 0,
+            ],
+        ];
+
+        // Loop through the date range and populate the counts array with the complaint counts
+        foreach (Carbon::now()->subDays(6)->toPeriod(Carbon::now()) as $date) {
+            $counts = 0;
+
+            foreach ($allComplaints as $complaint) {
+                if ($date->format("Y-m-d") == $complaint->date) {
+                    $counts += $complaint->count;
+                }
+            }
+
+            $axises['data']['yAxis'][] = $counts;
+            $axises['data']['xAxis'][] = $date->format("Y-m-d");
+        }
+
+        // Convert string to int
+        foreach ($genders as $gender => $value) {
+            $axises['genders'][$gender] = (int) $value;
+        }
+
+        return $axises;
+    }
 }
