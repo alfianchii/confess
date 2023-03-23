47205949c923738220980d335cec7a85997f7331
alfianchii <pianaee379@gmail.com>
2023-03-23 21:02:23 +0700

    chore: add directive asset

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -214,7 +214,7 @@
                             <div class="card-body py-4 px-4">
                                 <div class="d-flex align-items-center">
                                     <div class="avatar avatar-xl">
-                                        <img src="assets/images/faces/1.jpg" alt="Face 1" />
+                                        <img src="{{ asset('assets/images/faces/1.jpg') }}" alt="Face 1" />
                                     </div>
                                     <div class="ms-3 name">
                                         <h5 class="font-bold">{{ auth()->user()->name }}</h5>
@@ -516,7 +516,7 @@
                             <div class="card-body py-4 px-4">
                                 <div class="d-flex align-items-center">
                                     <div class="avatar avatar-xl">
-                                        <img src="assets/images/faces/1.jpg" alt="Face 1" />
+                                        <img src="{{ asset('assets/images/faces/1.jpg') }}" alt="Face 1" />
                                     </div>
                                     <div class="ms-3 name">
                                         <h5 class="font-bold">{{ auth()->user()->name }}</h5>
