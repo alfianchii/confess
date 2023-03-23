d1aece66b97a3a6d08cb14f89a5dc581bbe6a7b8
alfianchii <pianaee379@gmail.com>
2023-03-23 13:28:16 +0700

    feat: yourComplaintAxises() in Response

--- app/Models/Complaint.php
+++ app/Models/Complaint.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -57,5 +58,42 @@ class Complaint extends Model
     public function responses()
     {
         return $this->hasMany(Response::class);
+    }
+
+    public static function yourComplaintAxises()
+    {
+        // Your complaints
+        $complaints = Complaint::where("student_nik", auth()->user()->nik)
+            ->whereBetween('created_at', [Carbon::now()->subDays(7), Carbon::now()])
+            // Select the date of creation and count of complaints for each day
+            ->selectRaw("DATE(created_at) as date, COUNT(*) as count")
+            // Group the results by the date of creation
+            ->groupBy('date', "created_at")
+            // Order the results by date in ascending order
+            ->orderBy('date', 'asc')
+            // Execute the query and retrieve the results
+            ->get();
+
+        // Create an array to store the complaints counts for each day
+        $axises = [
+            'xAxis' => [],
+            'yAxis' => [],
+        ];
+
+        // Loop through the date range and populate the counts array with the complaints counts
+        foreach (Carbon::now()->subDays(6)->toPeriod(Carbon::now()) as $date) {
+            $counts = 0;
+
+            foreach ($complaints as $complaint) {
+                if ($date->format("Y-m-d") == $complaint->date) {
+                    $counts += $complaint->count;
+                }
+            }
+
+            $axises['yAxis'][] = $counts;
+            $axises['xAxis'][] = $date->format("Y-m-d");
+        }
+
+        return $axises;
     }
 }
