8a804d20cfa44e0bd86d0cc382fa894c3ee40cde
alfianchii <pianaee379@gmail.com>
2023-03-23 15:55:14 +0700

    feat: add DashboardService into DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,11 +7,12 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    protected $chartService;
+    protected $dashboardService, $chartService;
 
     // Constructor to add services
-    public function __construct(ChartService $chartService)
+    public function __construct(DashboardService $dashboardService, ChartService $chartService)
     {
+        $this->dashboardService = $dashboardService;
         $this->chartService = $chartService;
     }
 
