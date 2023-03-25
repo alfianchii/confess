c09c3954f880372d259d8d59c982cb30295812d2
alfianchii <pianaee379@gmail.com>
2023-03-25 11:49:40 +0700

    feat: sweetalert multi in /dashboard/categories

--- resources/js/sweetalert/swalMulti.js
+++ resources/js/sweetalert/swalMulti.js
@@ -34,4 +34,19 @@ if (currentPath === "/dashboard/complaints") {
                 );
             }
         });
+} else if (currentPath === "/dashboard/categories") {
+    document
+        .getElementById("table3")
+        .addEventListener("click", function (event) {
+            if (
+                event.target &&
+                event.target.classList.contains("delete-record")
+            ) {
+                handleDelete(
+                    event.target.dataset.slug,
+                    "kategori",
+                    "/dashboard/categories"
+                );
+            }
+        });
 }
