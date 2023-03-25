bc2ca1d3183996caef20ed6437c2ed283da2644b
alfianchii <pianaee379@gmail.com>
2023-03-25 11:50:47 +0700

    feat: sluggable in Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,6 +18,20 @@ class Category extends Model
     protected $guarded = [
         'id',
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function getRouteKeyName()
+    {
+        return "slug";
+    }
 
     public function complaints()
     {
