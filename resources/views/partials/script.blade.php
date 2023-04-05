7abe2b21a0cc4e917340153c33275f3ff5c1b6be
alfianchii <pianaee379@gmail.com>
2023-04-05 07:45:54 +0700

    chore: remove navbar.js in login page

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1,6 +1,9 @@
+{{-- Landing page --}}
 <script src="{{ asset('assets/static/js/components/dark.js') }}"></script>
 <script src="{{ asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
 <script src="{{ asset('assets/compiled/js/app.js') }}"></script>
 <script src="{{ asset('assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
 <script src="{{ asset('assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
-@vite('resources/js/navbar.js')
+@if (!Route::is('login'))
+    @vite('resources/js/navbar.js')
+@endif
