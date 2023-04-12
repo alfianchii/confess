7a74a1369e5ce103423a8ab8721e23e28f88b315
alfianchii <pianaee379@gmail.com>
2023-04-12 16:21:33 +0700

    chore: fix scripts

--- resources/views/dashboard/partials/script.blade.php
+++ resources/views/dashboard/partials/script.blade.php
@@ -1,4 +1,4 @@
 {{-- Base scripts --}}
-<script src="{{ asset('assets/compiled/js/app.js') }}"></script>
 <script src="{{ asset('assets/static/js/components/dark.js') }}"></script>
 <script src="{{ asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('assets/compiled/js/app.js') }}"></script>
