af559ee07bfe4987ec3ce100d65df9aa4fec31f1
alfianchii <pianaee379@gmail.com>
2023-04-12 14:30:51 +0700

    chore(ui): breadcrumb in profile and setting page

--- resources/views/dashboard/users/setting.blade.php
+++ resources/views/dashboard/users/setting.blade.php
@@ -27,15 +27,24 @@
                 <div class="col-12 col-md-6 order-md-2 order-first">
                     <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item">
-                                <a href="/dashboard">Dashboard</a>
-                            </li>
-                            <li class="breadcrumb-item">
-                                <a href="/dashboard/users">Users</a>
-                            </li>
-                            <li class="breadcrumb-item active" aria-current="page">
-                                Setting
-                            </li>
+                            @can('admin')
+                                <li class="breadcrumb-item">
+                                    <a href="/dashboard">Dashboard</a>
+                                </li>
+                                <li class="breadcrumb-item">
+                                    <a href="/dashboard/users">Users</a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    Profile
+                                </li>
+                            @else
+                                <li class="breadcrumb-item">
+                                    <a href="/dashboard">Dashboard</a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    Profile
+                                </li>
+                            @endcan
                         </ol>
                     </nav>
                 </div>
