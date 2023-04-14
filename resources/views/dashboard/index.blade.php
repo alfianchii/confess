73d3a6048045eef066c4544436e46a14b3bcd291
Surya <surya.nata.aardhana@gmail.com>
2023-04-14 20:13:48 +0700

    fix: card padding in mobile small view

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -725,7 +725,7 @@
                             </div>
                             <div class="col-6 col-lg-3 col-md-6">
                                 <div class="card">
-                                    <div class="card-body px-4 py-4-5">
+                                    <div class="card-body px-4 py-4-5" id="tanggapan-student">
                                         <div class="row">
                                             <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                                                 <div class="stats-icon red mb-2">
