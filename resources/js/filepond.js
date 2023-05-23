f25c1260a78bd3344fe321642deb837b279d0c09
alfianchii <pianaee379@gmail.com>
2023-05-23 15:56:59 +0700

    fix: multiple image when using filepond.js

--- resources/js/filepond.js
+++ resources/js/filepond.js
@@ -11,37 +11,45 @@ FilePond.registerPlugin(
 
 // Filepond: Image Crop
 export function imageCrop() {
-    FilePond.create(document.querySelector(".image-crop-filepond"), {
-        credits: null,
-        allowImagePreview: true,
-        allowImageFilter: false,
-        allowImageExifOrientation: false,
-        allowImageCrop: true,
-        acceptedFileTypes: ["image/png", "image/jpg", "image/jpeg"],
-        fileValidateTypeDetectType: (source, type) =>
-            new Promise((resolve, reject) => {
-                // Do custom type detection here and return with promise
-                resolve(type);
-            }),
-        storeAsFile: true,
-        imageCropAspectRatio: "1:1",
+    const images = document.querySelectorAll(".image-crop-filepond");
+
+    images.forEach((image) => {
+        FilePond.create(image, {
+            credits: null,
+            allowImagePreview: true,
+            allowImageFilter: false,
+            allowImageExifOrientation: false,
+            allowImageCrop: true,
+            acceptedFileTypes: ["image/png", "image/jpg", "image/jpeg"],
+            fileValidateTypeDetectType: (source, type) =>
+                new Promise((resolve, reject) => {
+                    // Do custom type detection here and return with promise
+                    resolve(type);
+                }),
+            storeAsFile: true,
+            imageCropAspectRatio: "1:1",
+        });
     });
 }
 
+// Filepond: Image Preview
 export function imagePreview() {
-    // Filepond: Image Preview
-    FilePond.create(document.querySelector(".image-preview-filepond"), {
-        credits: null,
-        allowImagePreview: true,
-        allowImageFilter: false,
-        allowImageExifOrientation: false,
-        allowImageCrop: false,
-        acceptedFileTypes: ["image/png", "image/jpg", "image/jpeg"],
-        fileValidateTypeDetectType: (source, type) =>
-            new Promise((resolve, reject) => {
-                // Do custom type detection here and return with promise
-                resolve(type);
-            }),
-        storeAsFile: true,
+    const images = document.querySelectorAll(".image-preview-filepond");
+
+    images.forEach((image) => {
+        FilePond.create(image, {
+            credits: null,
+            allowImagePreview: true,
+            allowImageFilter: false,
+            allowImageExifOrientation: false,
+            allowImageCrop: false,
+            acceptedFileTypes: ["image/png", "image/jpg", "image/jpeg"],
+            fileValidateTypeDetectType: (source, type) =>
+                new Promise((resolve, reject) => {
+                    // Do custom type detection here and return with promise
+                    resolve(type);
+                }),
+            storeAsFile: true,
+        });
     });
 }
