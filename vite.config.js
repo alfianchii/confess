84fd05ba3f730cf4bde873b0d84d1fc0a4d8620f
alfianchii <pianaee379@gmail.com>
2023-09-24 13:36:48 +0700

    fix: vite build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import fs from "fs";
+import { resolve } from "path";
+
+const root = resolve(__dirname, "resources");
+
+const resourceFiles = (dirPath, filesArray = []) => {
+    const files = fs.readdirSync(dirPath);
+
+    for (const file of files) {
+        if (file !== "views") {
+            const filePath = resolve(dirPath, file);
+            const fileStat = fs.statSync(filePath);
+            fileStat.isDirectory()
+                ? resourceFiles(filePath, filesArray)
+                : filesArray.push(filePath);
+        }
+    }
+
+    return filesArray;
+};
+
+const files = resourceFiles(root);
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: fs
-                .readdirSync("resources")
-                .filter((file) => fs.statSync(`resources/${file}`).isFile()),
-            refresh: true,
-        }),
-    ],
+    plugins: [laravel(files)],
 });
