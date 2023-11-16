4947e71d82fa04e816ca6c4039d211b7457381bd
alfianchii <pianaee379@gmail.com>
2023-11-16 21:02:03 +0700

    chore: vite w/ hostable and portable

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import fs from "fs";
 import path, { resolve } from "path";
 import { fileURLToPath } from "url";
 
+// Root path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const root = resolve(__dirname, "resources");
 
+// Load all of files on "views" folder
 const resourceFiles = (dirPath, filesArray = []) => {
     const paths = fs.readdirSync(dirPath);
 
@@ -24,9 +25,30 @@ const resourceFiles = (dirPath, filesArray = []) => {
 
     return filesArray;
 };
-
 const files = resourceFiles(root);
 
-export default defineConfig({
-    plugins: [laravel({ input: files, refresh: true })],
-});
+// Configs
+export default (mode) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.pwd()) };
+
+    return defineConfig({
+        // Server
+        server: {
+            // Serve all host
+            host: "0.0.0.0",
+            hmr: {
+                clientPort: process.env.VITE_APP_PORT,
+                host: process.env.VITE_APP_PORT,
+                // ws = websocket
+                protocol: "ws",
+            },
+            port: process.env.VITE_APP_PORT,
+            watch: {
+                usePolling: true,
+            },
+        },
+
+        // Plugins
+        plugins: [laravel({ input: files, refresh: true })],
+    });
+};
